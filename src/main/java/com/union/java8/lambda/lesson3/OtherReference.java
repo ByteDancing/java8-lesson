ab2002de055fbc99834be5dc540ae505f7f35bdd
ChengXiang <2393239440@qq.com>
2020-04-24 23:46:37 +0800

    其他引用

---
+++ src/main/java/com/union/java8/lambda/lesson3/OtherReference.java
@@ -0,0 +1,23 @@
+package com.union.java8.lambda.lesson3;
+
+import javax.sound.midi.Soundbank;
+import java.util.function.Function;
+
+/**
+ * description
+ *
+ * @author Mr.Cheng
+ * @date 2020/4/24
+ */
+public class OtherReference {
+    public static void main(String[] args) {
+        Function<Integer, String[]> function = x -> new String[x];
+        String[] apply = function.apply(10);
+        System.out.println(apply.length);
+
+
+        Function<Integer, String[]> function1 = String[]::new;
+        String[] apply1 = function1.apply(10);
+        System.out.println(apply1);
+    }
+}
